72696e405a265c14a41233c7b8ce024ab4e60496
Key Yu Wan <keyflcbyuwan@gmail.com>
2025-05-12 16:50:46 -0300

    fix: use state persistency instead of local storage on zustand

--- src/constants/storage/keys.ts
+++ src/constants/storage/keys.ts
@@ -1,2 +1,1 @@
 export const ORDERBOOK_STORAGE_KEY = '@nascent-frontend-test:orderbook'
-export const ORDERENTRY_STORAGE_KEY = '@nascent-frontend-test:orderentry'
--- src/store/order-entry/use-order-entry-store.ts
+++ src/store/order-entry/use-order-entry-store.ts
@@ -1,6 +1,4 @@
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
-import { ORDERENTRY_STORAGE_KEY } from '../../constants/storage/keys'
 
 interface OrderEntry {
   side: 'buy' | 'sell'
@@ -12,14 +10,7 @@ interface OrderEntryStore {
   setOrderEntry: (orderentry: Partial<OrderEntry>) => void
 }
 
-export const useOrderEntryStore = create(
-  persist<OrderEntryStore>(
-    set => ({
-      orderEntry: null,
-      setOrderEntry: data => set({ orderEntry: data }),
-    }),
-    {
-      name: ORDERENTRY_STORAGE_KEY,
-    }
-  )
-)
+export const useOrderEntryStore = create<OrderEntryStore>(set => ({
+  orderEntry: null,
+  setOrderEntry: data => set({ orderEntry: data }),
+}))
